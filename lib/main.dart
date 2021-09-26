5496b99f81a5d831dac6950b70b0f6943ff9b29c
stang <sahatsawat.xshot@mail.kmutt.ac.th>
2021-09-26 21:38:37 +0700

    Auto stash before merge of "development" and "origin/development"

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
-import 'package:washout/screens/customer/home_screen.dart';
 import 'package:washout/screens/general/app_entry.dart';
 import 'package:washout/configs/routes.dart';
+import 'package:washout/screens/merchant/manage_queue_screen.dart';
+import 'package:washout/screens/merchant/merchant_home_screen.dart';
+import 'package:washout/screens/register_screen.dart';
 import 'package:washout/screens/merchant/add_service_merchant_screen.dart';
 import 'package:washout/screens/merchant/merchant_home_screen.dart';
 import 'package:washout/screens/merchant/service_merchant_screen.dart';
@@ -11,7 +13,7 @@ import 'package:washout/screens/search_carwash_screen.dart';
 
 void main() => runApp(
       MaterialApp(
-      home: AddServiceMerchantScreen(),
+        home: ManageQueueScreen(),
         // initialRoute: AppEntry.routeName,
         debugShowCheckedModeBanner: false,
         // routes: routes,
