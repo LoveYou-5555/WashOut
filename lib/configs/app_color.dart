d85426cb520b2a8014aaa5f25f208cd4f465d4b7
athamin <athamin.angthnog@mail.kmutt.ac.th>
2021-11-29 02:58:50 +0700

    removeagain

--- lib/configs/app_color.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColor{
-  AppColor._();
-
-  static const customerPrimary = Color(0xFF2196F3);
-  static const merchantPrimary = Color(0xFFE63950);
-  
-}
